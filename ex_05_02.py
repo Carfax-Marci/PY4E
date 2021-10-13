df24172e59a60284b7d3d95fd8dd17124c4364c7
Carfax-Marci <marcel.westenberg@hotmail.de>
2021-10-13 21:13:23 +0200

    Update ex_05_02.py

--- ex_05_02.py
+++ ex_05_02.py
@@ -5,15 +5,18 @@ while True:
     sval = input('Enter a number: ')
     if sval == 'done':
         break
+    
     try:
         ival = int(sval)
     except:
         print('Invalid input')
         continue
+
     if largest is None:
         largest = ival
     if smallest is None:
         smallest = ival
+
     if ival > largest:
         largest = ival
     if ival < smallest:
